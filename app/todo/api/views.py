45597d9645ad208bcc547fc39ebe0ef1a69a0471
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-04 21:40:12 +0900

    feat: Todoリストビューを設定

---
+++ app/todo/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView # new
+from rest_framework.response import Response
+
+from todo.models import Todo
+from todo.api.serializers import TodoSerializer
+
+
+@api_view(['GET'])
+def todo_list_view(request):
+    """post_view"""
+    todos = Todo.objects.filter(author__id=request.id)
+    serializer = TodoSerializer(todos, many=True)
+    return Response(serializer.data)
