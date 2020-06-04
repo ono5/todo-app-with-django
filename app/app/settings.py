0ab3d1fa72b99b2ef22230b3834ba59fd32221bb
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-04 15:01:54 +0900

    fix: DBの設定値を環境変数から取得できるように変更

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'app',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-        'PASSWORD': 'supersecretpassword',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
