b288c372f6f17b0d8081e1ea3c59686a7a9c97ea
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-04 20:21:16 +0900

    feat: Todoモデルを追加

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    """Todo Model
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
