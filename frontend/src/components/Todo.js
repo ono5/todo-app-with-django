9c8eeec09f8df71889ab97f38a42af6be740a1b1
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-05 21:46:50 +0900

    feat: Todo作成処理を追加

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,25 +1,78 @@
-import React from 'react'
-
+import React, {
+    useContext,
+    useEffect,
+    useState
+} from 'react'
+import axios from 'axios'
 import {
     Button,
     Col,
 } from 'react-bootstrap';
+import AppContext from '../contexts/AppContext';
 
 const Todo = () => {
+    const { state, dispatch } = useContext(AppContext)
+    const [id, setId] = useState(0)
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+
+    useEffect(() => {
+        // userIDを取得
+        axios.get('http://localhost/api/account/', {
+            headers: {
+                'Authorization': state.user.token,
+            }})
+            .then(data => {
+                setId(data.data.id)
+            })
+    })
+
+    const submitPost = (e) => {
+        e.preventDefault()
+
+        const data = {
+            author: 1,
+            title: title,
+            content: content
+        }
+        axios.post('http://localhost/api/todos/', data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': state.user.token
+            }})
+            .then(res => {
+                window.location.href = '/'
+            })
+    }
+
+    // タイトルとコンテンツの入力チェック
+    const unPost = title === '' || content === ''
+
     return (
         <Col xl={3}>
             <h4>To-Do App</h4>
             <form>
                 <div className="form-group">
                     <label htmlFor="formTodoTitle">タイトル</label>
-                    <input className="form-control" id="formTodoTitle" />
+                    <input
+                      className="form-control"
+                      id="formTodoTitle"
+                      onChange={e => setTitle(e.target.value)}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="formTodoBody">内容</label>
-                    <textarea className="form-control" id="formTodoBody" />
+                    <textarea
+                      className="form-control"
+                      id="formTodoBody"
+                      onChange={e => setContent(e.target.value)}
+                    />
                 </div>
-                <Button className="btn btn-primary">予定を追加する</Button>
-                <Button className="btn btn-danger">全ての予定を削除する</Button>
+                <Button
+                  className="btn btn-primary"
+                  onClick={submitPost}
+                  disabled={unPost}>予定を追加する</Button>
+                <Button className="btn btn-danger m-4">全ての予定を削除する</Button>
             </form>
         </Col>
     )
--- frontend/src/components/TodoContainer.js
+++ frontend/src/components/TodoContainer.js
@@ -1,27 +1,25 @@
 import React, {useEffect, useReducer} from 'react'
 import {
-    Row,
+    Row
 } from 'react-bootstrap';
 import Todo from './Todo';
 import TodoList from './TodoList';
 import reducer from '../reducers'
 import { SET_USER } from '../actions';
+import Navibar from './Navibar';
 
 const TodoContainer = () => {
     const [state, dispatch] = useReducer(reducer, {})
-    useEffect(() => {
-        if (!sessionStorage.getItem('username')) {
-            window.location.href = '/login'
-        }
-    }, [])
-
     return (
 
-        <div className="App container-flud">
+        <div className="App container-flud" id="Todo-area">
             <header className="App-header">
+                <Row>
+                    <Navibar />
+                </Row>
                 <Row className="App-row">
-                <Todo />
-                <TodoList />
+                  <Todo />
+                  <TodoList />
                 </Row>
             </header>
         </div>
