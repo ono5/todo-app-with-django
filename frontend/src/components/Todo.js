15b81a5a2bb2c2e6b4189086305dcc3bb4b572d9
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-07 06:04:07 +0900

    refactor: 不要なモジュールの読み込みを削除

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,6 +1,5 @@
 import React, {
     useContext,
-    useEffect,
     useState
 } from 'react'
 import AppContext from '../contexts/AppContext';
--- frontend/src/components/TodoContainer.js
+++ frontend/src/components/TodoContainer.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer} from 'react'
+import React from 'react'
 import {
     Row
 } from 'react-bootstrap';
@@ -11,10 +11,10 @@ const TodoContainer = () => {
 
         <div className="App container-flud" id="Todo-area">
             <header className="App-header">
+                <Row className="App-row">
                 <Row>
                     <Navibar />
                 </Row>
-                <Row className="App-row">
                   <TodoForm />
                   <TodoList />
                 </Row>
