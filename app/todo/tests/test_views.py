758be58f9fc90c85dbff91906c83aafb00ecd6c6
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-06-07 19:07:41 +0900

    test: Todoリスト取得テストを追加

--- app/todo/tests/test_views.py
+++ app/todo/tests/test_views.py
@@ -6,18 +6,34 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
-from app.todo.models import Todo
+from todo.models import Todo
+from todo.api.serializers import TodoSerializer
 
 # django-rest-frameworkのViewSetでは、XXXX-listでURL取得可能
 TODO_URL = reverse('todo:todos-list')
 
 
-class TodoViewSetTests(TestCase):
+def sample_todo(user, **params):
+    """Todoを作成する
+    """
+    defaults = {
+        'title': 'First Todo',
+        'content': 'Hello World',
+    }
+    defaults.update(params)
+    return Todo.objects.create(author=user, **defaults)
+
+
+class TodoViewSetAuthTests(TestCase):
     """TodoViewSetのTestクラス
     """
 
     def setUp(self):
         self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'admin',
+            'adminpass'
+        )
 
     def test_auth_required_ng(self):
         """TodoViewSetの認証テスト
@@ -37,11 +53,7 @@ class TodoViewSetTests(TestCase):
         200OKが返却される
         """
         # Arrange ---
-        user = get_user_model().objects.create_user(
-            'admin',
-            'adminpass'
-        )
-        token = Token.objects.create(user=user)
+        token = Token.objects.create(user=self.user)
         client = self.client
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
@@ -50,3 +62,30 @@ class TodoViewSetTests(TestCase):
 
         # Assert ---
         assert res.status_code == status.HTTP_200_OK
+
+
+class TodoViewSetTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'admin',
+            'adminpass'
+        )
+        token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_retrieve_todos(self):
+        """Todoリストを取得する
+        """
+        # Arrange ---
+        sample_todo(user=self.user)
+        sample_todo(user=self.user)
+
+        # Act ---
+        res = self.client.get(TODO_URL)
+
+        # Assert ---
+        todos = Todo.objects.all().order_by('id')
+        serializer = TodoSerializer(todos, many=True)
+        assert res.status_code == status.HTTP_200_OK
+        assert res.data == serializer.data
